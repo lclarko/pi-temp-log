506d72121ea10cc9c464bb0b3811c0363a58c0da
Lane <lane.clark@gov.bc.ca>
2020-08-01 12:29:57 -0700

    Initial commit

---
+++ pi_temp_log.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Script: pi_temp_log.sh
+# Use: Log the ARM CPU and GPU temperatures of your Raspberry Pi 4
+# ASCII art adapted from user b3n on raspberrypi.org
+# Tested on: LibreELEC 9.X.X
+###########
+echo "
+   .~~.   .~~.        Raspberry Pi 4
+  '. \ ' ' / .'         GPU + CPU
+   .~ .~~~..~.      Temperature Monitor
+  : .~.'~'.~. :
+ ~ (   ) (   ) ~
+( : '~'.~.'~' : )
+ ~ .~ (   ) ~. ~
+  (  : '~' :  )
+   '~ .~~~. ~'
+       '~'"
+# Path to LOG_FILE
+LOG_FILE=/storage/pi_temp.log
+# Set CPU Temp as variable
+cpu=$( cat /sys/class/thermal/thermal_zone0/temp)
+echo "$(date) @ $(hostname)" | tee -a ${LOG_FILE}
+echo "-------------------------------------------"
+echo "GPU     -  CPU   -  TIME"
+while true; do
+# Return GPU and CPU temp in celcius + 8601 timestamp
+echo "$(/opt/vc/bin/vcgencmd measure_temp | cut -d = -f2)  -  $((cpu/1000))'C  -  $(date -I'seconds')" | tee -a ${LOG_FILE}
+# Frequency of temperature measurement in seconds
+sleep 3
+done
