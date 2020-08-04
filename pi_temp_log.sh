836d1de54dec15c14b68d25e1aed15023b601d21
Lane <lane.clark@gov.bc.ca>
2020-08-03 18:07:05 -0700

    - Check for no log file & create it - Added better comments - Changed log file location to current dir

--- pi_temp_log.sh
+++ pi_temp_log.sh
@@ -17,16 +17,23 @@ echo "
   (  : '~' :  )
    '~ .~~~. ~'
        '~'"
-# Path to LOG_FILE
-LOG_FILE=/storage/pi_temp.log
-# Set CPU Temp as variable
+
+# Path to LOG_FILE and log file creation
+LOG_FILE=./pi_temp.log
+if [[ ! -f "./pi_temp.log" ]]
+then touch pi_temp.log && echo "Created pi_temp.log"
+fi
+
+# Assign CPU temp to variable for future calculation
 cpu=$( cat /sys/class/thermal/thermal_zone0/temp)
 echo "$(date) @ $(hostname)" | tee -a ${LOG_FILE}
 echo "-------------------------------------------"
 echo "GPU     -  CPU   -  TIME"
 while true; do
+
 # Return GPU and CPU temp in celcius + 8601 timestamp
 echo "$(/opt/vc/bin/vcgencmd measure_temp | cut -d = -f2)  -  $((cpu/1000))'C  -  $(date -I'seconds')" | tee -a ${LOG_FILE}
-# Frequency of temperature measurement in seconds
+
+# Temperature measurement interval
 sleep 3
 done
